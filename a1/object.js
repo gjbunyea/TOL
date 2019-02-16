349645ec2da19bcaf7cec040e2123c1f279c28f1
Greg Bunyea <gjbunyea@buffalo.edu>
2019-02-16 13:30:48 -0500

    button loading

--- a1/object.js
+++ a1/object.js
@@ -3,7 +3,8 @@ let scenes = [
         "id": 0,
         "name": "start",
         "begin": 0,
-        "timespan": 10000,
-        "next-scene": [1,2] 
+        "timespan": 3000,
+        "nextscene": [1,2], 
+        "buttonopts": ["This is a test button","Here is another test button"]
     }
 ]
--- a1/run-script.js
+++ a1/run-script.js
@@ -2,9 +2,21 @@ $(document).ready(function () {
     $("#button-container").hide();
 });
 
-function run(playerStatus, state){
-    currentScene = getScene(state);
-    console.log(currentScene.name)
-    setTimeout(pauseVideo, currentScene.timespan)
- }
+function run(playerStatus, state) {
+    currentScene = getScene(state)
+    // console.log(currentScene.name)
+    setTimeout(sceneOver, currentScene.timespan, currentScene)
+}
 
+function sceneOver(scene) {
+    console.log(scene.name)
+
+    pauseVideo()
+
+    $("button").each(function (index) {
+        // console.log()
+        $( this ).html(scene.buttonopts[index])
+    });
+
+    $("#button-container").show("slow")
+}
