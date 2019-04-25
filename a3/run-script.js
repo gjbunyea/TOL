1b781e4ba68b2d9e820b06575da9787745ca0d25
Greg Bunyea <gjbunyea@buffalo.edu>
2019-04-25 00:50:38 -0400

    1 question test, successful

--- a3/run-script.js
+++ a3/run-script.js
@@ -1,50 +1,8 @@
 $(document).ready(function () {
     $("#button-container").hide();
-    var scene = getScene("ques1");
-    run(scene)
+    q = getQuestion("ques1")
+    makeQuestion(q)
 });
-
-function run(currentScene) {
-    setTimeout(sceneOver, currentScene.timespan, currentScene)
-}
-
-function sceneOver(scene) {
-
-    pauseVideo()
-
-    var nextQ = getQuestion(scene.nextQ)
-
-    if (nextQ.name == "ques3" && q2flag == true) {    
-        makeQuestion(getQuestion("ques2"))
-    } else {
-        makeQuestion(nextQ)
-    }
-    // $("#questionContainer").show("slow")
-}
-
-function nextScene(name) {
-
-    if (name.startsWith("ques")) {
-
-        if (name == "ques3" && q2flag == true) {
-        
-            makeQuestion(getQuestion("ques2"))
-        
-        } else {
-            var nextQ = getQuestion(name)
-            makeQuestion(nextQ)
-        }
-
-    } else {
-
-            var next = getScene(name)
-
-            $("#questionContainer").hide(1000)
-            run(next)
-
-        
-    }
-}
 
 function makeQuestion(question) {
 
@@ -62,30 +20,18 @@ function makeButtons(buttons) {
     buttons.forEach(b => {
         $("<button/>", {
             id: b.id,
-            text: b.description,
+            text: b.desc,
         })
             .attr("onClick", "giveFeedback(\"" + b.answer + "\",\"" + b.feedback + "\",\"" + b.whereTo + "\")")
             .appendTo("#buttonContainer")
-
-        if (b.image != "") {
-            $("<img>", { src: b.image }).appendTo("#" + b.id)
-        }
     });
 }
 
 function giveFeedback(cor, words, whereTo) {
 
-    if(words.endsWith("again soon.")){
-        q2flag = true
-    }
-    if (q2flag && words.endsWith("difference is.")){
-        whereTo = "ques3"
-        q2flag = false
-    }
-
     $("#questionContainer").hide()
 
-    if (cor == 'true') {
+    if (cor == 1) { //  1 is correct, 0 is incorrect
 
         $("<h1/>", { text: "Correct!" })
             .css('background-color', '#99ff99')
@@ -109,5 +55,4 @@ function clearFeedback(whereTo) {
 
     $("#feedbackContainer").empty()
 
-    nextScene(whereTo)
 }
